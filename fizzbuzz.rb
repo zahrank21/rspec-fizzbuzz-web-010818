90520bf090084ada1324e17672e9802db248964e
zahrank21 <zahrank@gmail.com>
2017-12-23 20:13:03 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+
+  def fizzbuzz(int)
+
+  if ((int % 3 == 0) && (int % 5 != 0)) == true
+    return "Fizz"
+  elsif ((int % 5 == 0) && (int % 3 != 0)) == true
+    return "Buzz"
+  elsif ((int % 3 == 0) && (int % 5 == 0)) == true
+    return "FizzBuzz"
+  end
+  nil
+  end
